94c0c52193b525e0e820cddf63aeab359bd96c70
Sergej Derjabkin <sergej@derjabkin.de>
2013-06-22 20:02:19 +0200

    - Initial commit

---
+++ VSAssemblyResolver/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace SergejDerjabkin.VSAssemblyResolver
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidPopulateToolbox =        0x100;
+
+
+    };
+}
